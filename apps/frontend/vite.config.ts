693a4b4468cf68ebad972c48941ce40f7f7df8fa
Ethan Snow <bombitmanbomb@gmail.com>
2025-02-27 02:11:18 -0500

    setup build stage

--- apps/frontend/vite.config.ts
+++ apps/frontend/vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vite.dev/config/
 export default defineConfig({
   appType: "spa",
-  build: { outDir: "../build" },
+  build: { outDir: "./build" },
   plugins: [react()],
   server: {
     port: 3000,
